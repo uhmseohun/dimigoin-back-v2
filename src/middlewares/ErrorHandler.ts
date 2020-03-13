27bfd1a691ceac93fae9c852a5a7217f07ea9c5c
uhmtoto <uhmtotow@gmail.com>
2020-03-13 15:45:21 +0900

    fix: set default status code

--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import HttpException from '../exceptions/HttpException';
 
 const errorHandler = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
-  const { status, message } = error;
+  const { status = 500, message } = error;
   res.status(status).json({ message });
 };
 
