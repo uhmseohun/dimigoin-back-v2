983bc015ea166dd3331ef1dfc855701f12d1820a
uhmtoto <uhmtotow@gmail.com>
2020-03-13 16:03:38 +0900

    feat: implement AccessDeniedException

---
+++ src/exceptions/Permission.ts
@@ -0,0 +1,10 @@
+import HttpException from './HttpException';
+
+export class AccessDeniedException extends HttpException {
+  constructor(status: number = 401,
+              message: string = '권한이 부족합니다.') {
+    super(status, message);
+    this.status = status;
+    this.message = message;
+  }
+}
