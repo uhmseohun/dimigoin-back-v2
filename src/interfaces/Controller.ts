abe092b5a711f672b9913a4b9f94675d13288828
uhmtoto <uhmtotow@gmail.com>
2020-03-13 16:47:04 +0900

    feat: implement getUserIdentity in Contoller class

--- src/interfaces/Controller.ts
+++ src/interfaces/Controller.ts
@@ -1,13 +1,23 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import Token from '../resources/Token';
+
+const TokenManager = new Token();
 
 const asyncWrapper = (fn: Function) =>
   (req: Request, res: Response, next: NextFunction) =>
     fn(req, res, next).catch(next);
 
+const getUserIdentity = (req: Request) => {
+  const { token } = req;
+  const identity = TokenManager.verify(token);
+  return identity;
+};
+
 class Controller {
   public basePath: string;
   public router: Router;
   protected wrapper = asyncWrapper;
+  protected getUserIdentity = getUserIdentity;
 }
 
 export default Controller;
