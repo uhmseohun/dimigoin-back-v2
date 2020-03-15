b4eac26f456612b4e3463f1be4bafda398b894e9
uhmtoto <uhmtotow@gmail.com>
2020-03-15 15:04:01 +0900

    feat: log errors only when develop node_env

--- src/middlewares/ErrorHandler.ts
+++ src/middlewares/ErrorHandler.ts
@@ -2,7 +2,10 @@ import { NextFunction, Request, Response } from 'express';
 import HttpException from '../exceptions/HttpException';
 
 const errorHandler = (error: HttpException, req: Request, res: Response, next: NextFunction) => {
-  console.error(error);
+  if (process.env.NODE_ENV === 'develop') {
+    console.error(error);
+  }
+
   const { status = 500, message } = error;
   res.status(status).json({ message });
 };
