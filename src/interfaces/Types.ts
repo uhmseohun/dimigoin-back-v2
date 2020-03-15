8652a3240b4606742dd2886393c0a7b70eb2c702
uhmtoto <uhmtotow@gmail.com>
2020-03-15 13:33:15 +0900

    refactor: type defining of grade and class

--- src/interfaces/Types.ts
+++ src/interfaces/Types.ts
@@ -12,6 +12,12 @@ export type Gender = typeof GenderValues[number];
 export const UserTypeValues = ['S', 'O', 'D', 'T', 'P'] as const;
 export type UserType = typeof UserTypeValues[number];
 
+export const GradeValues = [1, 2, 3] as const;
+export type Grade = typeof GradeValues[number];
+
+export const ClassValues = [1, 2, 3, 4, 5, 6] as const;
+export type Class = typeof ClassValues[number];
+
 export const CircleApplicationStatusValues =
   ['applied', 'interview', 'pass', 'final', 'fail'] as const;
 // 지원 완료 | 면접 대상 | 최종 합격 | 최종 선택 | 탈락
