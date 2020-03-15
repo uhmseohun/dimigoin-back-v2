7ff4e316ba6589be4116c62d5bb6d4b86590c9e4
uhmtoto <uhmtotow@gmail.com>
2020-03-15 17:43:49 +0900

    refactor: add user static methods

--- src/controllers/User.ts
+++ src/controllers/User.ts
@@ -28,13 +28,12 @@ class UserController extends Controller {
   }
 
   private getAllStudents = async (req: Request, res: Response, next: NextFunction) => {
-    // userType 'S'에는 졸업생도 포함되어 있어서 학년으로 재학생을 찾아야 함.
-    const students = await UserModel.find({ grade: { $gte: 1, $lte: 3 } });
+    const students = await UserModel.findStudents();
     res.json({ students });
   }
 
   private getAllTeachers = async (req: Request, res: Response, next: NextFunction) => {
-    const teachers = await UserModel.find({ userType: { $in: ['T', 'D'] } });
+    const teachers = await UserModel.findTeachers();
     res.json({ teachers });
   }
 
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,5 @@
 import { createSchema, Type, typedModel } from 'ts-mongoose';
-import { ClassValues, GradeValues, UserTypeValues } from '../types';
+import { ClassValues, GradeValues, UserTypeValues, UserType } from '../types';
 
 const userSchema = createSchema({
   idx: Type.number({ required: true, unique: true }),
@@ -15,6 +15,16 @@ const userSchema = createSchema({
 const UserModel = typedModel('User', userSchema, undefined, undefined, {
   findByIdx (idx: number) {
     return this.find({ idx });
+  },
+  findByUserType (userType: UserType[]) {
+    return this.find({ userType: { $in: userType } });
+  },
+  findStudents () {
+    // userType 'S'에는 졸업생도 포함되어 있어서 학년으로 재학생을 찾아야 함.
+    return this.find({ grade: { $gte: 1, $lte: 3 } });
+  },
+  findTeachers () {
+    return this.findByUserType(['D', 'T']);
   }
 });
 
