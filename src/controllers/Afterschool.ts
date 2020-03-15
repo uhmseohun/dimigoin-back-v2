0851704f666271af3a0436ee3a564879b5f8eab6
uhmtoto <uhmtotow@gmail.com>
2020-03-15 16:45:49 +0900

    feat: add afterschool get route

--- src/controllers/Afterschool.ts
+++ src/controllers/Afterschool.ts
@@ -1,4 +1,8 @@
 import { Controller } from '../interfaces';
+import { Request, Response, NextFunction } from 'express';
+import { CheckUserType } from '../middlewares';
+import { AccessDeniedException } from '../exceptions/Permission';
+import { AfterschoolModel } from '../models';
 
 class AfterschoolController extends Controller {
   public basePath = '/afterschool';
@@ -8,7 +12,27 @@ class AfterschoolController extends Controller {
     this.initializeRoutes();
   }
 
-  private initializeRoutes() {}
+  private initializeRoutes() {
+    this.router.get('/', CheckUserType(['T']),
+      this.wrapper(this.getAllAfterschools));
+    this.router.get('/:grade', CheckUserType(['S', 'T']),
+      this.wrapper(this.getAfterschoolsByGrade));
+  }
+
+  private getAllAfterschools = async (req: Request, res: Response, next: NextFunction) => {
+    const afterschools = await AfterschoolModel.find();
+    res.json({ afterschools });
+  }
+
+  private getAfterschoolsByGrade = async (req: Request, res: Response, next: NextFunction) => {
+    const user = this.getUserIdentity(req);
+    const grade = Number(req.params.grade);
+    if (user.userType === 'S' && user.grade !== grade) {
+      throw new AccessDeniedException();
+    }
+    const afterschools = await AfterschoolModel.find({ grade });
+    res.json({ afterschools });
+  }
 }
 
 export default AfterschoolController;
