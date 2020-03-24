a67361ee786fa2f3c2c9e7da8e98f5d8f657c04b
hanukoon <hwymaster01@gmail.com>
2020-03-24 18:30:02 +0900

    feat: add get circle by id (#30)

--- src/controllers/Circle.ts
+++ src/controllers/Circle.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import { Controller } from '../classes'
 import { CircleModel } from '../models'
 import Route from '../resources/RouteGenerator'
+import mongoose from 'mongoose'
 
 class CircleController extends Controller {
   public basePath = '/circle';
@@ -13,12 +14,19 @@ class CircleController extends Controller {
 
   private initializeRoutes () {
     this.router.get('/', Route(['S', 'T'], this.requiredKeys.none, this.getAllCircles))
+    this.router.get('/:circleId', Route(['S', 'T'], this.requiredKeys.none, this.getOneCircles))
   }
 
   private getAllCircles = async (req: Request, res: Response, next: NextFunction) => {
     const circles = await CircleModel.find()
       .populate('chair', ['name', 'serial'])
       .populate('viceChair', ['name', 'serial'])
+    res.json({ circles })
+  }
+
+  private getOneCircles = async (req: Request, res: Response, next: NextFunction) => {
+    const circleId = mongoose.Types.ObjectId(req.params.circleId)
+    const circles = await CircleModel.find({ _id: circleId })
     res.json({ circles })
   }
 }
