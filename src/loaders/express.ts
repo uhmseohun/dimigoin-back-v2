894b04d768376058a30d8d2d03fa5681961ffbdc
Lee Seungmin <lee@seungmin.io>
2020-04-16 12:17:38 +0900

    fix: update cors setting for cookie (#69)
    * feat: setting for cookie
    
    * fix: update cors setting for cookie

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 export default ({ app }: { app: express.Application }) => {
-  app.use(cors({ credentials: true }));
+  app.use(cors({ origin: 'https://circle.dimigo.in', credentials: true }));
 
   app.get('/status', (req, res) => {
     res.status(200).end();
